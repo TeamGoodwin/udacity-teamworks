9dced41a983199e558a84296d38f9621da1d6294
mkelly <mkelly@M-MacBook-Pro-2.local>
2017-09-24 12:45:04 +0100

    Fixed compiler error in the crosshair script

--- Assets/FPS KIT FREE/Scripts/Crosshair_Script.js
+++ Assets/FPS KIT FREE/Scripts/Crosshair_Script.js
@@ -4,7 +4,7 @@ var crosshair : Texture2D;
 function OnGUI () {
 	var w = crosshair.width/2;
 	var h = crosshair.height/2;
-	position = Rect((Screen.width - w)/2,(Screen.height - h )/2, w, h);
+	var position = Rect((Screen.width - w)/2,(Screen.height - h )/2, w, h);
 
 	if (!Input.GetButton ("Fire2")) { 
 		GUI.DrawTexture(position, crosshair);
