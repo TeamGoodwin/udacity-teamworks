b1c23409e54202b5c3a9983e2fbc4ff7b31d31c9
Michael Kelly <dev@luminaudio.com>
2017-09-24 12:30:00 +0100

    Linked ball direction to gun position and angle.  Added repeat rate control for continuous fire.

--- Assets/Scripts/BallShooter.cs
+++ Assets/Scripts/BallShooter.cs
@@ -2,29 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class BallShooter : MonoBehaviour {
+
+	// Defaults
+	private const float defaultRpm = 600;
+
 
 	public GameObject ballPrefab;
 	public float shootSpeed = 20;
-	public float shootOffset = 1.0f;
 	public bool singleShot = true;
 	public bool continuous = false;
+	public GameObject m_spawnPoint;
+	public float repeatRateRpm = defaultRpm;
+
 
 	private GameObject ball;
-
+	private float fireCountdown = 0.0f;
+	private float shotTime;
 	// Use this for initialization
 	void Start () {
-		
+		ReInitVars ();
 	}
 
 	private bool isFiring(){
 		bool fireButton = false;
+
+		// Are we continueous firing?
 		if (continuous) {
-			if (Input.GetButton ("Fire1")) {
-				fireButton = true;
+
+			// Check the repeat rate
+			if (Input.GetButton ("Fire1")){
+				print (fireCountdown);
+				// Subtract time whilst we are holding fire
+				fireCountdown -= Time.deltaTime;
+				if (fireCountdown <= 0.0f) {
+					// Fire!
+					fireButton = true;
+
+					// Restart the countdown if we just fired
+					fireCountdown += shotTime;
+				}
 			}
 		} else { 
 			if (Input.GetButtonDown ("Fire1")) {
+				// Fire!
 				fireButton = true;
 			}
 		}
@@ -37,13 +60,24 @@ public class BallShooter : MonoBehaviour {
 		if(isFiring())
 		{
 			ball = Instantiate (ballPrefab);
-			Camera camera = GetComponentInChildren<Camera> ();
-			Vector3 offset = new Vector3(0.0f, shootOffset, 0.0f);
-			ball.transform.position = camera.transform.position - offset;
+			ball.transform.position = m_spawnPoint.transform.position;
 			Rigidbody ballRb = ball.GetComponentInChildren<Rigidbody> ();
-			ballRb.velocity = camera.transform.rotation * Vector3.forward * shootSpeed;
+			ballRb.velocity = m_spawnPoint.transform.rotation * Vector3.forward * shootSpeed;
 		}
 	}
 
+	void OnValidate()
+	{
+		if (repeatRateRpm == 0.0f)
+			repeatRateRpm = defaultRpm;
+		if (repeatRateRpm < 0.0f)
+			repeatRateRpm = Mathf.Abs(repeatRateRpm);
+		ReInitVars ();
+	}
+
+	private void ReInitVars()
+	{
+		shotTime = 60.0f / repeatRateRpm;
+	}
 
 }
