8f70bef91a0ee9acc6029ee5ae419725ba33d95b
Pedro Corvo <pedrocorvo@Pedros-MacBook-Pro.local>
2017-09-27 23:06:31 +0100

    Added paint completion logic/reward sound. Added particle system to signal when object has been painted.

--- Assets/Scripts/PaintballHit.cs
+++ Assets/Scripts/PaintballHit.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class PaintballHit : MonoBehaviour {
 
+	public AudioClip completeClip;
+
 	float _colorComplete = 0.0f;
+	Renderer _renderer;
+	ParticleSystem _particles;
+	bool _painted = false;
+
+	void Awake()
+	{
+		_renderer = GetComponent<Renderer> ();
+		_particles = GetComponent<ParticleSystem> ();
+	}
 
 	// Steal the colour of the colliding object
     void OnCollisionEnter(Collision collision)
     {
         GameObject obj = collision.gameObject;
         Color col = obj.GetComponent<Renderer>().material.color;
-        GetComponent<Renderer>().material.color = col;
+        _renderer.material.color = col;
 
 		_colorComplete = 1.0f;
         gameObject.layer = LayerMask.NameToLayer("Outline");
@@ -19,8 +30,19 @@ public class PaintballHit : MonoBehaviour {
 
 	public void LaserHit(Color col)
 	{
-		_colorComplete += 0.001f;
-		Color newCol = Color.Lerp (GetComponent<Renderer> ().material.color, col, _colorComplete);
-		GetComponent<Renderer>().material.color = newCol;
+		_colorComplete += 0.01f;
+		Color newCol = Color.Lerp (_renderer.material.color, col, _colorComplete);
+		_renderer.material.color = newCol;
+	}
+
+	void Update()
+	{
+		if (_colorComplete >= 0.5f && !_painted) {
+			ParticleSystem.MainModule main = _particles.main;
+			main.startColor = _renderer.material.color;
+			_particles.Play ();
+			AudioSource.PlayClipAtPoint (completeClip, transform.position);
+			_painted = true;
+		}
 	}
 }
