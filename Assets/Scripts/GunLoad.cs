29d11adee692d88e53c93610c32820a5a0a7e025
Masa Iketani <masa.iketani@gmail.com>
2017-10-01 02:09:43 +0900

    Added Start Scene

---
+++ Assets/Scripts/GunLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int gunPicked = SceneControl.gun;
+		Debug.Log (gunPicked);
+		GameObject uzi = GameObject.Find ("Uzi");
+		GameObject M200 = GameObject.Find ("M200");
+		GameObject SPAS = GameObject.Find ("SPAS");
+
+		switch (gunPicked) {
+		case 0:
+			Destroy (M200);
+			Destroy (SPAS);
+			break;
+		case 1:
+			Destroy (uzi);
+			Destroy (SPAS);
+			break;
+		case 2:
+			Destroy (uzi);
+			Destroy (M200);
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour {
+	public static int gun = 0;
+
+	public void LoadSceneWithUzi () {
+		SceneManager.LoadScene ("ColorPrototype");
+	}
+
+	public void LoadSceneWithM200 () {
+		gun = 1;
+		SceneManager.LoadScene ("ColorPrototype");
+	}
+
+	public void LoadSceneWithSpas () {
+		gun = 2;
+		SceneManager.LoadScene ("ColorPrototype");
+	}
+}
