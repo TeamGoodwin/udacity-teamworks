56f0173cc3de9d035449ac9718b70bd00fc62bb6
Pedro Corvo <pedrocorvo@Pedros-MacBook-Pro.local>
2017-10-01 23:10:13 +0100

    Adding colour pallete

---
+++ Assets/ColourPallete.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourPallete : MonoBehaviour {
+
+	public Color[] colours;
+
+	public Color GetColour()
+	{
+		return colours[Random.Range(0, colours.Length)];
+	}
+}
